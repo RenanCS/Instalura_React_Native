779020da708b91e5e04953ce860c0b275f2d8262
RenanConstrutiva <renan.carvalho@construtiva.com.br>
2018-10-22 12:00:20 -0300

    Ajustes de funcionamento e troca de versão

--- InstaluraMobile/src/screens/Login/Login.js
+++ InstaluraMobile/src/screens/Login/Login.js
@@ -1,30 +1,56 @@
 import React, { Component } from "react";
 import {
     StyleSheet,
-    View,
-    Image,
-    TouchableOpacity,
-    TextInput 
+  Text,
+  View,
+  Image,
+  Dimensions,
+  FlatList,
+  TouchableOpacity,
+  TextInput,
+  Button
 } from "react-native";
 
+const width = Dimensions.get('screen').width;
 
 export default class Login extends Component {
 
     render(){
         return(
-            <View>
-                <Text placeholder="Usuário..."
-                onChangeText={texto => this.setState({usuario:texto})}></Text>
+            <View style={styles.container}>
+            <Text style= {styles.titulo}>Instalura</Text>
+            <View style={styles.form}>
+                <TextInput style={styles.input} 
+                    placeholder="UsuÃ¡rio..."
+                    onChangeText={texto => this.setState({usuario: texto})}/>
 
-                  <Text placeholder="Senha..."
-                onChangeText={texto => this.setState({senha:texto})}></Text>
+                <TextInput style={styles.input} 
+                    placeholder="Senha..."
+                    onChangeText={texto => this.setState({senha: texto})}/>
 
+                <Button title="Login" onPress={() => console.warn("Login")} />
+                </View>
             </View>
         );
     }
-
-
-
-
-
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    titulo: {
+        fontWeight: 'bold',
+        fontSize: 26,
+    },
+    form: {
+        width: width * 0.8
+    },
+    input: {
+        height: 40,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd'
+    }
+})
