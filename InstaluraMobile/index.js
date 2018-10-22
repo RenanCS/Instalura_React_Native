a57f81c5502697b078b0f41bce6dadd2bcb2706c
RenanConstrutiva <renan.carvalho@construtiva.com.br>
2018-10-22 17:27:17 -0300

    Ajustes

--- InstaluraMobile/index.js
+++ InstaluraMobile/index.js
@@ -1,8 +1,17 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
+//import {AppRegistry} from 'react-native';
+import {createStackNavigator,} from 'react-navigation';
+  
 import Feed from './src/screens/Feed/index';
 import Login from './src/screens/Login/Login';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Login);
+
+const App = createStackNavigator({
+    Login: { screen: Login },
+    Feed: { screen: Feed },
+  });
+
+export default App;
+// AppRegistry.registerComponent(appName, () => Login);
